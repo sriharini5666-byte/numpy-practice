6dc9235588a5bd992ae11d3ba4b5b7c6a7e06715
Sri Harini <sriharini5666@gmail.com>
2026-09-18 22:45:27 +0530

    Added JavaScript variables and data types

---
+++ javascript-practice/variables.js
@@ -0,0 +1,20 @@
+let name = "Sri Harini";
+let age = 19;
+
+console.log(name);
+console.log(age);
+
+age = 20;
+console.log(age);
+
+const college = "Mailam Engineering College";
+console.log(college);
+
+var city = "Tindivanam";
+console.log(city);
+
+if (age >= 18) {
+    console.log("Eligible");
+} else {
+    console.log("not eligible");
+}
