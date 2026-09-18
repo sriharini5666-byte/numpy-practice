ba63d8514ec54d27a121f37176d0cc2e067546ce
Sri Harini <sriharini5666@gmail.com>
2026-09-18 23:03:45 +0530

    Updated JavaScript data types

---
+++ javascript-practice/datatypes.js
@@ -0,0 +1,43 @@
+// String
+let name = "Sri Harini";
+console.log(name);
+console.log(typeof name);
+
+// Number
+let age = 20;
+console.log(age);
+console.log(typeof age);
+
+// Boolean
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+// Undefined
+let fees;
+console.log(fees);
+console.log(typeof fees);
+
+// Null
+let mark = null;
+console.log(mark);
+console.log(typeof mark);
+
+// BigInt
+let bigNumber = 120335678890123459999n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+// Symbol
+let id = Symbol("student");
+console.log(id);
+console.log(typeof id);
+
+// Object
+let student = {
+    name: "Sri Harini",
+    age: 20
+};
+
+console.log(student);
+console.log(typeof student);
